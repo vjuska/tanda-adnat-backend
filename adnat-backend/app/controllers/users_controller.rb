20b77b439dc83f1a7db966949547506cd2be67be
Jesseppi <jesse.creech@gmail.com>
2019-10-14 21:07:47 +1000

    sign in form now has basic validation on first name, last name and email address

--- adnat-backend/app/controllers/users_controller.rb
+++ adnat-backend/app/controllers/users_controller.rb
@@ -1,18 +1,26 @@
 class UsersController < ApplicationController
 
+    def index
+        @users = User.all
+    end
+
     def show
         @user = User.find(params[:id])
     end
 
     def new
+        @user = User.new
     end
 
 
     def create
-    @user = User.new(article_params)
+        @user = User.new(article_params)
 
-    @user.save
-    redirect_to @user
+        if @user.save
+            redirect_to @user
+        else
+            render 'new'
+        end
     end
 
 
--- adnat-backend/app/models/user.rb
+++ adnat-backend/app/models/user.rb
@@ -1,2 +1,18 @@
-class User < ApplicationRecord
+
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not a valid email")
+    end
+  end
 end
+
+class User < ApplicationRecord
+
+    validates :first_name, presence: true, length: {minimum: 2}
+    validates :last_name, presence: true, length: {minimum: 2}
+    validates :email_address, presence: true, email: true
+
+end
+
+
