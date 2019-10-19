05723aa55b88dda6bb76e53aff24735e84920f2a
Jesseppi <jesse.creech@gmail.com>
2019-10-20 09:58:57 +1000

    Form is now a template
    Users can now be deleted

--- adnat-backend/app/controllers/users_controller.rb
+++ adnat-backend/app/controllers/users_controller.rb
@@ -13,11 +13,11 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find([:id])
+        @user = User.find(params[:id])
     end
 
     def create
-        @user = User.new(article_params)
+        @user = User.new(user_params)
 
         if @user.save
             redirect_to @user
@@ -27,17 +27,24 @@ class UsersController < ApplicationController
     end
 
     def update
-        @user = User.find([:id])
-        if @user.update(article_params)
+        @user = User.find(params[:id])
+        if @user.update(user_params)
             redirect_to @user
         else
             render 'edit'
         end
     end
 
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+ 
+        redirect_to users_path
+    end
+
 
     private
-     def article_params
+     def user_params
         params.require(:user).permit(:first_name,:last_name,:email_address,:password,:passwordconfirmation)
      end
 end
