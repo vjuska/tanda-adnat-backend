9f1cf19049ac14b4d6c019084052f410ba0bc262
Jesseppi <jesse.creech@gmail.com>
2019-10-20 11:34:29 +1000

    start password validation

--- adnat-backend/app/models/user.rb
+++ adnat-backend/app/models/user.rb
@@ -1,4 +1,5 @@
 
+
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
@@ -7,12 +8,18 @@ class EmailValidator < ActiveModel::EachValidator
   end
 end
 
-class User < ApplicationRecord
+class UserValidator < ActiveModel::Validator
+   def validate(record)
+      print "here is my password" + record.password
+      if record[:password] != record[:passwordConfirmation]
+        record.errors[:base] << "The passwords do not match"
+      end
+   end
+end
 
+class User < ApplicationRecord
     validates :first_name, presence: true, length: {minimum: 2}
     validates :last_name, presence: true, length: {minimum: 2}
     validates :email_address, presence: true, email: true
-
+    validates_with UserValidator
 end
-
-
