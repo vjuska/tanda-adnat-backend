f453cf92f027cb25ce5517c70748cde5f31d52e3
JesseC <jesse.creech@gmail.com>
2019-11-09 16:04:57 +1000

    start user session
    cookie is saving, redirection is not

--- adnat-backend/config/routes.rb
+++ adnat-backend/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   get 'sign_up/index'
   get 'login/index'
 
@@ -7,6 +10,11 @@ Rails.application.routes.draw do
   resources :employees
   resources :shifts
 
-  root 'login#index'
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  root 'organisations#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
